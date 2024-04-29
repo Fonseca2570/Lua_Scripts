0cfc79c390a0a1811ac34d3e143912bfc8d78f90
Fonseca2570 <56457144+Fonseca2570@users.noreply.github.com>
2024-04-29 22:42:28 +0100

    Update ClueTesting.lua

--- ClueTesting.lua
+++ ClueTesting.lua
@@ -13,6 +13,7 @@ local Bosses = {
         InsideDoor = 26425,
         Boss = 6260,
         Minions = {6261,6263,6265},
+        SumBetweenDoors = 6,
         Drops = {
             995, --coins
             25022, -- Bandos Helmet
@@ -56,8 +57,20 @@ local States = {
 
 local currentState = States.Banking
 local SelectedBoss = Bosses.Bandos
+local startXp = API.GetSkillXP("CONSTITUTION")
 
 -- GameState checks and idle
+local function checkXpIncrease() 
+    local newXp = API.GetSkillXP("CONSTITUTION")
+    if newXp == startXp then 
+        API.logError("no xp increase")
+        currentState = States.Banking
+    else
+        startXp = newXp
+    end
+end
+
+
 local function idleCheck()
     local timeDiff = os.difftime(os.time(), afk)
     local randomTime = math.random((MAX_IDLE_TIME_MINUTES * 60) * 0.6, (MAX_IDLE_TIME_MINUTES * 60) * 0.9)
@@ -65,6 +78,7 @@ local function idleCheck()
     if timeDiff > randomTime then
         API.PIdle2()
         afk = os.time()
+        checkXpIncrease()
         return true
     end
 end
@@ -141,61 +155,67 @@ local function PressPortal()
 end
 
 local function PressOutsideDoor() 
-    API.RandomSleep2(1800,200,300)
-    API.DoAction_Object1(0x31, API.OFF_ACT_GeneralObject_route0, { SelectedBoss.OutsideDoor },50)
-    API.RandomSleep2(1800,200,300)
-    API.WaitUntilMovingEnds()
+    if ClueUtils.AtLocation(SelectedBoss.Locations.Outside) then 
+        API.RandomSleep2(1800,200,300)
+        API.DoAction_Object1(0x31, API.OFF_ACT_GeneralObject_route0, { SelectedBoss.OutsideDoor },50)
+        API.RandomSleep2(1800,200,300)
+        API.WaitUntilMovingEnds()
+    end
 end
 
-local function PressInsideDoor() 
-    if fail > 3 then 
-        API.Write_LoopyLoop(false)
-        return
-    end
-    API.DoAction_Object1(0x31, API.OFF_ACT_GeneralObject_route0, { SelectedBoss.InsideDoor },50)
-    API.RandomSleep2(600,200,300)
-    API.WaitUntilMovingEnds()
-    if API.Compare2874Status(12, false) then 
-        API.KeyboardPress2(0x32, 0, 50) -- press 2 for instance encounter
-        API.RandomSleep2(600,100,300)
-        if API.Compare2874Status(18, false) then 
-            -- Instance system is open
-            fail = 0
+local function PressInsideDoor()
+    if ClueUtils.AtLocation(SelectedBoss.Locations.BetweenDoors) then 
+        if fail > 3 then 
+            API.Write_LoopyLoop(false)
             return
+        end
+        API.DoAction_Object1(0x31, API.OFF_ACT_GeneralObject_route0, { SelectedBoss.InsideDoor },50)
+        API.RandomSleep2(600,200,300)
+        API.WaitUntilMovingEnds()
+        if API.Compare2874Status(12, false) then 
+            API.KeyboardPress2(0x32, 0, 50) -- press 2 for instance encounter
+            API.RandomSleep2(600,100,300)
+            if API.Compare2874Status(18, false) then 
+                -- Instance system is open
+                fail = 0
+                return
+            else
+                fail = fail + 1 
+                PressInsideDoor()
+            end
+            
         else
             fail = fail + 1 
             PressInsideDoor()
         end
-        
-    else
-        fail = fail + 1 
-        PressInsideDoor()
     end
 end
 
 local function StartNewInstance() 
     if API.Compare2874Status(18, false) then 
         API.DoAction_Interface(0x24,0xffffffff,1,1591,60,-1,API.OFF_ACT_GeneralInterface_route)
-        API.RandomSleep2(1800,0,0)
-        if API.GetAllObjArray1({SelectedBoss.InsideDoor}, 1, {12}) then 
-            API.DoAction_Object1(0x31, API.OFF_ACT_GeneralObject_route0, { SelectedBoss.InsideDoor },50)
-            API.RandomSleep2(600,200,300)
-            API.WaitUntilMovingEnds()
-            currentState = States.InsideInstance
-        end
+        API.RandomSleep2(2600,500,1000)
+    end
+
+    if API.GetAllObjArray1({SelectedBoss.InsideDoor}, 1, {12}) then 
+        API.DoAction_Object1(0x31, API.OFF_ACT_GeneralObject_route0, { SelectedBoss.InsideDoor },50)
+        API.RandomSleep2(600,200,300)
+        API.WaitUntilMovingEnds()
+        currentState = States.InsideInstance
     end
 end
 
 local function RejoinLastInstance() 
     if API.Compare2874Status(18, false) then 
         API.DoAction_Interface(0x24,0xffffffff,1,1591,122,-1,API.OFF_ACT_GeneralInterface_route)
-        API.RandomSleep2(1800,0,0)
-        if API.GetAllObjArray1({SelectedBoss.InsideDoor}, 1, {12}) then 
-            API.DoAction_Object1(0x31, API.OFF_ACT_GeneralObject_route0, { SelectedBoss.InsideDoor },50)
-            API.RandomSleep2(600,200,300)
-            API.WaitUntilMovingEnds()
-            currentState = States.InsideInstance
-        end
+        API.RandomSleep2(2600,500,1000)
+    end
+
+    if API.GetAllObjArray1({SelectedBoss.InsideDoor}, 1, {12}) then 
+        API.DoAction_Object1(0x31, API.OFF_ACT_GeneralObject_route0, { SelectedBoss.InsideDoor },50)
+        API.RandomSleep2(600,200,300)
+        API.WaitUntilMovingEnds()
+        currentState = States.InsideInstance
     end
 end
 
@@ -278,6 +298,7 @@ local function Bank()
     if timerHitZero() and CleanBossRoom() then 
         if TeleportToWarRetreat() then 
             currentState = States.Banking
+            instanceStart = nil
             API.RandomSleep2(1200,0,300)
             API.WaitUntilMovingandAnimEnds()
             return
@@ -286,6 +307,85 @@ local function Bank()
 end
 -- end boss functions
 
-PressPortal()
-PressOutsideDoor()
-PressInsideDoor()
+-- try to figure out current state
+local function UpdateState() 
+    if not API.IsPlayerMoving_() and not API.CheckAnim(100) then 
+        if ClueUtils.AtLocation(SelectedBoss.Locations.Outside) or ClueUtils.AtLocation(SelectedBoss.Locations.BetweenDoors) then 
+            currentState = States.GoingToBoss
+            return
+        end
+
+        if ClueUtils.AtLocation(ClueUtils.Locations.WarRetreat) then 
+            currentState = States.Banking
+            return
+        end
+
+        local inside = API.GetAllObjArray1({SelectedBoss.InsideDoor}, 10, {12})[1]
+        local outside = API.GetAllObjArray1({SelectedBoss.OutsideDoor}, 10, {12})[1]
+        
+        if inside ~= nil and outside ~= nil then 
+            if inside.Distance + outside.Distance == SelectedBoss.SumBetweenDoors then 
+                currentState = States.GoingToBoss
+                return
+            end
+        end
+        currentState = States.InsideInstance
+        return
+    end
+end
+
+while API.Read_LoopyLoop() do
+    UpdateState()
+    API.DoRandomEvents()
+    idleCheck()
+    gameStateChecks()
+    API.Write_ScripCuRunning0(currentState)
+
+    if currentState == States.Banking then 
+        if not ClueUtils.AtLocation(ClueUtils.Locations.WarRetreat) then 
+            TeleportToWarRetreat()
+            API.RandomSleep2(3000,500,1000)
+        end
+
+        LoadLastPreset()
+        API.RandomSleep2(1000,500,1000)
+        API.WaitUntilMovingEnds()
+
+        if API.GetPrayPrecent() < 90 then 
+            API.DoAction_Object1(0x3d,API.OFF_ACT_GeneralObject_route0,{ClueUtils.MostUsedIDS.WarAltar} ,50)
+            API.RandomSleep2(600,0,0)
+            API.WaitUntilMovingandAnimEnds()
+            API.RandomSleep2(1200,0,0)
+        end
+
+        if API.GetHPrecent() > 90 then 
+            PressPortal()
+            API.RandomSleep2(1000,500,1000)
+            API.WaitUntilMovingEnds()
+        end
+        goto continue
+    end
+
+    if currentState == States.GoingToBoss then 
+        PressOutsideDoor()
+        API.RandomSleep2(1200,100,300)
+        PressInsideDoor()
+        API.RandomSleep2(1200,100,300)
+        EnterInstance()
+        API.RandomSleep2(1200,100,300)
+        goto continue
+    end
+
+    if currentState == States.InsideInstance then 
+        ClueUtils.IncreasePrayerPoints()
+        ClueUtils.EatFood()
+        ClueUtils.ActivatePrayer(ClueUtils.Abilities.SoulSplit, ClueUtils.BuffBar.soulsplit)
+        PrioritizeBoss()
+        Bank()
+        Loot()
+        goto continue
+    end
+
+    ::continue::
+    API.RandomSleep2(600,0,200)
+end
